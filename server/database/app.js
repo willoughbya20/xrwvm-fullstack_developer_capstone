bed21ead0c9ef99f7f5c954026cb4e0e49f0433e
willoughbya20 <adampwilloughby@gmail.com>
2024-06-17 12:36:56 -0400

    Modified app.js in database to server API endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -56,19 +56,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
   }
 });
 
+
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        let documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers' });
+    }
 });
+
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    let formattedState = req.params.state[0].toUpperCase();
+    formattedState += req.params.state.slice(1);
+    try {
+        let documents = await Dealerships.find({state: formattedState});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        let documents = await Dealerships.find({id: req.params.id});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealers by id'});
+    }
 });
 
 //Express route to insert review
